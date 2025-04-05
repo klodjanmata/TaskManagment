9bc2fffae14628d0e38e7c839aa46e8397be9fd6
Natalia024nako <natalianako1@icloud.com>
2025-04-05 14:18:41 +0200

    project1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,41 @@
+import Entity.ProjectEntity;
+import Entity.ProjectRepository;
+import com.google.protobuf.TextFormat;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
 public class Main {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        Configuration configure = configuration.configure("hibernate.cfg.xml");
+        SessionFactory sessionFactory = configuration
+                .buildSessionFactory();
+        ProjectRepository pr = new ProjectRepository();
+        ProjectEntity project = new ProjectEntity();
+        project.setName("Ana");
+        project.setDiscription("Makajsjhjgyguy");
+        final SimpleDateFormat dateFormater = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+
+            Date startDate = parse("2020-02-04");
+            Date endDate = parse("2021-02-04");
+            project.setDateOfStart(startDate);
+            project.setDateOfEnd(endDate);
+            pr.save(project);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        pr.save(project);
+    }
+
+    private static Date parse(String date) {
+        return null;
+    }
+
 }
