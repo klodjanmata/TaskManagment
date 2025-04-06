0832d035cde5bce5908aef39588d0b9bf278453a
Natalia024nako <natalianako1@icloud.com>
2025-04-06 14:35:33 +0200

    project2 and more

--- src/main/java/Service/ProjectService.java
+++ src/main/java/Service/ProjectService.java
@@ -25,113 +25,59 @@ public class ProjectService {
 
     }
 
-    private void createProject(Project project) {
-
-    }
-
 
     public void updateProject() {
-        System.out.print("Enter project ID to update");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        Project project = new Project();
-        project.setId(projectId);
-        System.out.print("Enter new project name");
-        project.setName(scanner.nextLine());
-        System.out.print("Enter new description");
-        project.setDiscription(scanner.nextLine());
-        projectRepository.save(project);
-        System.out.print("Project is updated");
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter your id"));
+        p.setName(Helper.getStringFromUser("Enter new project name"));
+        p.setDiscription(Helper.getStringFromUser("Enter new discription"));
+        projectRepository.save(p);
+        System.out.println("Project is updated");
+
     }
 
 
-    public static void deleteProject() {
-        scanner.nextLine();
-        System.out.print("Enter project ID to delete");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        Project project = new Project();
-        project.setId(projectId);
-        projectManagement.deleteProject(project);
+    public void deleteProject() {
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter project ID to delete"));
+        projectRepository.save(p);
         System.out.println("Project is deleted");
 
     }
 
-    private void deleteProject(Project project) {
-    }
 
-
-    public static void assignEmployeesToProject() {
-        System.out.print("Enter project ID: ");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        Project project = new Project();
-        project.setId(projectId);
-        System.out.println("Enter employee IDs to assign");
-        String employeeId = scanner.nextLine();
-        List<Employees> employees = getEmployeesById(employeeId);
-        projectManagement.assignEmployeesToProject(project, employees);
+    public void assignEmployeesToProject() {
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter project ID"));
+        p.setId(Helper.getIntFromUser("Enter employee Id to assign"));
+        projectRepository.save(p);
         System.out.println("Employees assigned to project");
-    }
 
-    private void assignEmployeesToProject(Project project, List<Employees> employees) {
-    }
-
-    private static List<Employees> getEmployeesById(String employeeId) {
-        return null;
     }
 
 
-    public static List<Employees> viewEmployeesAssignedToProject() {
-        System.out.print("Enter project ID to view employees: ");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        Project project = new Project();
-        project.setId(projectId);
-        List<Employees> employees = projectManagement.viewEmployeesAssignedToProject(project);
+    public final List<Employees> viewEmployeesAssignedToProject() {
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter project ID to view employees: "));
+        projectRepository.save(p);
         System.out.println("Employees assigned to project:");
-        for (Employees employee : employees) {
-            System.out.println(employee);
-        }
-        return employees;
     }
 
 
-    public static void viewAllTasksByProject() {
-        System.out.print("Enter project Id to view all tasks");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        Project project = new Project();
-        project.setId(projectId);
-        List<Task> tasks = projectManagement.viewAllTasksByProject(project);
+    public void viewAllTasksByProject() {
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter project Id to view all tasks"));
+        projectRepository.save(p);
         System.out.println("Tasks associated to the project:");
-        for (Task task : tasks) {
-            System.out.println(task);
-        }
+
     }
 
-    private List<Task> viewAllTasksByProject(Project project) {
-        return null;
-    }
 
-    public static void generateTaskReportByProject() {
-        System.out.print("Enter project ID to generate task report");
-        int projectId = scanner.nextInt();
-        scanner.nextLine();
-        List<Task> tasks = projectService.generateTaskReportByProject(projectId);
+    public void generateTaskReportByProject() {
+        Project p = new Project();
+        p.setId(Helper.getIntFromUser("Enter project ID to generate task report"));
+        projectRepository.save(p);
         System.out.println("Project-wise task report:");
-        for (Task task : tasks) {
-            System.out.println(task);
-        }
-    }
-
-    private List<Task> generateTaskReportByProject(int projectId) {
-
-        return null;
-    }
-
-
-    private static Date parseDate(String startDate) {
-        return null;
     }
 }
+
